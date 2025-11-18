18f79406b589aaa9e379c3d3a5fedf5e6227e61f
himanshu aaithan <himanshukumawat9649@gmail.com>
2025-11-18 13:58:02 +0530

    everything changed

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-var con = document.querySelector(".post")
-var love = document.querySelector("i")
+let img = document.querySelector(".img img");
+let love = document.querySelector("#love")
+let likes = document.querySelector(".likecount h1")
+let counnt = 2750;
+let heart = document.querySelector("#heart")
 
-con.addEventListener("dblclick", function () {
+likes.innerHTML = `${counnt} likes`
+
+img.addEventListener("dblclick", function () {
+    if (counnt === 2750) {
+        counnt = counnt + 1
+    }
     love.style.transform = "translate(-50%, -50%) scale(1)"
-    love.style.color = "red"
-    love.style.opacity = 0.8
+    heart.innerHTML = `<i class="ri-heart-fill"></i>`
+    love.style.opacity = 1
+    heart.style.color = "red"
+    likes.innerHTML = `${counnt} likes`
     setTimeout(function () {
         love.style.opacity = 0
-    }, 1000);
+    }, 1000)
     setTimeout(function () {
         love.style.transform = "translate(-50%, -50%) scale(0)"
-    }, 2000);
-});
+    }, 2000)
+})
+
+
+heart.addEventListener("click", function () {
+    if (counnt === 2751) {
+        counnt--
+        likes.innerHTML = `${counnt} likes`
+    }
+
+    heart.innerHTML = ` <i  class="ri-heart-line"></i>`
+    heart.style.color = "black"
+    
+})
+
+
